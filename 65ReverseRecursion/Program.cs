2f77a9c4a20615cc5b06f9a967eb6b061feb859c
Tatyana <tatyana.bazh17@gmail.com>
2022-11-12 17:33:21 +0400

    решены 2 из 3-х д/з задач на Семинар №9 Рекурсии C#

---
+++ 65ReverseRecursion/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+string ReverseGetPrint(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return end + " " + ReverseGetPrint(start, end - 1);
+}
+
+Console.Write("Введите натуральное число N: ");
+int number = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(ReverseGetPrint(m, number));
---
+++ 68FunctionAkkermanRecursion/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    
+        return 1 + FunctionAkkerman(m - 1, n);
+    
+    // else if (m > 0 && n > 0)
+    // {
+    //     return ;
+    // }
+}
+
+Console.Write("Введите целое неотрицательное число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите целое неотрицательное число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Функция Аккермана для {m} и {n} = {FunctionAkkerman(m,n)}");
